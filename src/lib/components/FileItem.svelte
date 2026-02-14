50dae53b8b05e399123cab97bb27b917a76daa12
n3tael <netael@proton.me>
2026-02-15 00:13:37 +0200

    redesign fileitem and fix progress styles for chrome-based browsers

--- src/lib/components/FileItem.svelte
+++ src/lib/components/FileItem.svelte
@@ -9,44 +9,52 @@
 		button_disabled,
 		on_item_remove
 	}: { item: QueueItem; button_disabled: boolean; on_item_remove: () => void } = $props();
+
+	function download() {
+		const a = document.createElement('a');
+		a.style.display = 'none';
+		a.download = item.output_file?.name ?? '';
+		a.href = item.output_file?.url ?? '#';
+
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+	}
 </script>
 
 <div class="file" transition:slide>
-	<Music class="m-1 mr-2" />
+	<Music class="m-1 mr-2 shrink-0" size="16" />
 
-	<div class="flex gap-2 items-center mr-auto">
-		<span>{item.input_file.name}</span>
+	<div class="flex items-center overflow-hidden mr-auto">
+		<span class="whitespace-nowrap overflow-hidden text-ellipsis mr-2">{item.input_file.name}</span>
+		
+		{#if item.status === QueueItemStatus.PROCESSING}
+		<span class="text-muted text-sm" in:fade out:slide={{ axis: 'x' }}>
+			{Math.round(item.progress.current)}%
+		</span>
+		{/if}
+
 		{#if item.status === QueueItemStatus.DONE}
 			<Check size="16" />
 		{:else if item.status === QueueItemStatus.ERROR}
 			<CircleX size="16" />
 		{/if}
-
-		{#if item.output_file}
-			<a href={item.output_file.url} download={item.output_file.name}>
-				<button class="ghost">
-					<Download size="16" />
-				</button>
-			</a>
-		{/if}
 	</div>
 
 	{#if item.status === QueueItemStatus.PROCESSING}
-		<progress
-			transition:fade
-			class="absolute left-0 right-0 top-0 bottom-0 bg-transparent z-0"
-			value={item.progress.current}
-			max="100"
-		></progress>
-		<span transition:fade class="text-neutral-500 text-center absolute left-0 right-0"
-			>{Math.round(item.progress.current)}%</span
-		>
+		<progress transition:fade value={item.progress.current} max="100"></progress>
 	{/if}
 
-	<span class="text-neutral-500 mr-2">{filesize(item.input_file.size)}</span>
+	<span class="text-muted mx-2 shrink-0 text-xs">{filesize(item.input_file.size)}</span>
+
+	{#if item.output_file}
+		<button onclick={download} class="ghost">
+			<Download size="16"/>
+		</button>
+	{/if}
 
 	{#if !button_disabled}
-		<button onclick={on_item_remove} transition:slide={{ axis: 'x' }} class="ghost"><X /></button>
+		<button onclick={on_item_remove} transition:slide={{ axis: 'x' }} class="ghost shrink-0"><X size="16" /></button>
 	{/if}
 </div>
 
@@ -54,18 +62,24 @@
 	@reference "$styles";
 
 	.file {
-		@apply border-border bg-secondary relative my-1 flex items-center gap-0 overflow-clip rounded-xl border-1 p-2 shadow-sm;
+		@apply border-border bg-secondary relative my-1 flex items-center overflow-clip rounded-xl border p-2 shadow-sm;
 
 		:not(progress) {
 			@apply z-5;
 		}
+	}
 
-		progress {
-			@apply z-0 bg-transparent;
-		}
+	progress,
+	::-webkit-progress-bar {
+		@apply absolute left-0 right-0 w-full h-full bg-transparent z-0;
+		-webkit-appearance: none;
 	}
 
 	::-moz-progress-bar {
+		@apply bg-foreground/10;
+	}
+	
+	::-webkit-progress-value {
 		@apply bg-foreground/10;
 	}
 </style>
